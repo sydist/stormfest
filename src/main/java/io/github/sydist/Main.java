58293b2bbf715f25c6ddedbe5b64b10faf14830a
Sydist <sydistbot.personal@gmail.com>
2022-09-06 00:43:56 +0200

    build: initialize fabric project

---
+++ src/main/java/io/github/sydist/Main.java
@@ -0,0 +1,11 @@
+package io.github.sydist;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Main implements ModInitializer {
+
+	@Override
+	public void onInitialize() {
+        System.out.println("Hello World!");
+    }
+}
